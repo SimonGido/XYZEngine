70e2f056a3d3df2f34c9bbb1436128f6049ce39d
Šimon Gido <simongido1@gmail.com>
2021-02-09 14:19:37 +0100

    memory pool

--- XYZEngine/src/XYZ/Core/MemoryPool.cpp
+++ XYZEngine/src/XYZ/Core/MemoryPool.cpp
@@ -4,4 +4,73 @@
 
 namespace XYZ {
 
+	MemoryPool::MemoryPool(size_t blockSize)
+		:
+		m_BlockSize(blockSize)
+	{
+	}
+
+	MemoryPool::~MemoryPool()
+	{
+		if (m_Block.Data)
+			delete[]m_Block.Data;
+	}
+
+	void MemoryPool::mergeFreeChunks()
+	{
+		std::sort(m_FreeChunks.begin(), m_FreeChunks.end(), [](const Chunk& a, const Chunk& b) {
+			return a.Index < b.Index;
+			});
+		for (auto it = m_FreeChunks.begin(); it != m_FreeChunks.end(); ++it)
+		{
+			for (auto it2 = it + 1; it2 != m_FreeChunks.end();)
+			{
+				// Chunks are connected so merge them
+				if (it->Index + it->Size == it2->Index || it2->Index + it2->Size == it->Index)
+				{
+					it->Index = std::min(it2->Index, it->Index);
+					it->Size = it->Size + it2->Size;
+					it2 = m_FreeChunks.erase(it2);
+					continue;
+				}
+				else
+				{
+					// Chunks are not connected after their indices where sort so continue to check other chunks
+					break;
+				}
+				it2++;
+			}
+		}
+	}
+	size_t MemoryPool::findAvailableIndex(size_t size)
+	{
+		uint32_t counter = 0;
+		for (auto& chunk : m_FreeChunks)
+		{
+			if (chunk.Size >= size + sizeof(size_t))
+			{
+				Chunk tmp = chunk;
+				m_FreeChunks.erase(m_FreeChunks.begin() + counter);
+				return tmp.Index;
+			}
+			else if (counter == m_FreeChunks.size() - 1)
+			{
+				// If there is not occupied memory after the last free chunk, uset he last chunk space together with not occupied memory
+				if (chunk.Index + size == m_NextAvailableIndex && chunk.Index + size < m_BlockSize)
+				{
+					Chunk tmp = chunk;
+					m_FreeChunks.erase(m_FreeChunks.begin() + counter);
+					m_NextAvailableIndex = tmp.Index + size;
+					return tmp.Index;
+				}
+			}
+			counter++;
+		}
+
+		size_t index = m_NextAvailableIndex;
+		m_NextAvailableIndex += size + sizeof(size_t);
+		*(size_t*)&m_Block.Data[index + size] = index; // Store into user data index of the chunk
+
+		return index;
+	}
 }
--- XYZEngine/src/XYZ/Core/MemoryPool.h
+++ XYZEngine/src/XYZ/Core/MemoryPool.h
@@ -1,6 +1,66 @@
 #pragma once
-
+#include <cstdint>
+#include <vector>
 
 namespace XYZ {
 
+	class MemoryPool
+	{
+	private:
+		struct Block
+		{
+			uint8_t* Data = nullptr;
+		};
+		struct Chunk
+		{
+			size_t Size = 0;
+			size_t Index = 0;
+		};
+
+	public:
+		MemoryPool(size_t blockSize);
+		~MemoryPool();
+
+		template <typename T, typename ...Args>
+		T* Allocate(Args&&... args)
+		{
+			if (!m_Block.Data)
+			{
+				m_Block.Data = new uint8_t[m_BlockSize];
+				memset(m_Block.Data, 0, m_BlockSize);
+			}
+			size_t index = findAvailableIndex(sizeof(T));
+			return new((void*)&m_Block.Data[index])T(std::forward<Args>(args)...);
+		}
+
+		template <typename T>
+		void Deallocate(T* val)
+		{
+			uint8_t* tmp = (uint8_t*)val + sizeof(T);
+			size_t index = *(size_t*)tmp;
+
+			Chunk chunk{ sizeof(T) + sizeof(size_t), index };
+			// Removed chunk is at the end of used memory so just move available index backwards
+			if (chunk.Index + chunk.Size == m_NextAvailableIndex)
+				m_NextAvailableIndex -= chunk.Index + chunk.Size;
+			else
+			{
+				m_FreeChunks.push_back(chunk);
+				mergeFreeChunks();
+			}
+		}
+	private:
+		void mergeFreeChunks();
+		size_t findAvailableIndex(size_t size);
+
+	private:
+		const size_t m_BlockSize;
+
+		size_t m_NextAvailableIndex = 0;
+
+		// TODO: Multiple blocks
+		Block m_Block;
+
+		std::vector<Chunk> m_FreeChunks;
+	};
 }
