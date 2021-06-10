167b970913f9283bb285ab4b523b9ccae62a8366
Šimon Gido <simongido1@gmail.com>
2021-06-10 22:25:55 +0200

    update

--- XYZEngine/src/XYZ/Particle/ParticleModule.h
+++ XYZEngine/src/XYZ/Particle/ParticleModule.h
@@ -18,48 +18,15 @@ namespace XYZ {
 		ParticleShaderDataType Type;
 	};
 
-	// TODO: Maybe you are going to use thath
-	template <size_t ElementCount>
 	struct ParticleModule
 	{
-		std::string			  Name;
-		ParticleModuleElement Elements[ElementCount];
-		bool				  Enabled;
+		std::string						   Name;
+		std::string						   SourceCode;
+		std::vector<ParticleModuleElement> Elements;
+		bool							   Enabled;
 
-		std::string ToStruct() const;
+		void LoadFromFile(const std::string& filepath);
+		
+		std::string ElementsToStruct() const;
 	};
-
-	template<size_t ElementCount>
-	inline std::string ParticleModule<ElementCount>::ToStruct() const
-	{
-		std::string result;
-		result.append("struct " + Name + "\n{\n");
-		for (size_t i = 0; i < ElementCount; ++i)
-		{
-			switch (Elements[i].Type)
-			{
-			case ParticleShaderDataType::Int:
-				result.append("int ");
-				break;
-			case ParticleShaderDataType::UInt:
-				result.append("uint ");
-				break;
-			case ParticleShaderDataType::Float:
-				result.append("float ");
-				break;
-			case ParticleShaderDataType::Float2:
-				result.append("vec2 ");
-				break;
-			case ParticleShaderDataType::Float3:
-				result.append("vec3 ");
-				break;
-			case ParticleShaderDataType::Float4:
-				result.append("vec4 ");
-				break;
-			}
-			result.append(Elements[i].Name + ";\n");
-		}
-		result.append("\n};\n");
-		return result;
-	}
 }
