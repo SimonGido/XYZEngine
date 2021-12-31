5b83b11692c04bdd3d2d415d728ef5919ec853b1
SimonGido <44289637+SimonGido@users.noreply.github.com>
2021-12-31 11:10:56 +0100

    Update StateMachine.h

--- XYZEngine/src/XYZ/FSM/StateMachine.h
+++ XYZEngine/src/XYZ/FSM/StateMachine.h
@@ -7,114 +7,58 @@ namespace XYZ {
 
 	
 	template <uint32_t NumStates>
-	class State
-	{
-	public:
-		State() = default;
-		State(uint32_t id, const std::bitset<NumStates>& allowedTransitions)
-			: m_ID(id), m_AllowedTransitionsTo(allowedTransitions)
-		{}
-
-		void SetAllowedTransitions(const std::bitset<NumStates>& allowedTransitionsToBitset)
-		{
-			m_AllowedTransitionsTo = allowedTransitionsToBitset;
-		}
-		void AllowTransition(uint32_t stateIndex)
-		{
-			m_AllowedTransitionsTo |= BIT(stateIndex);
-		}
-		void DisallowTransition(uint32_t stateIndex)
-		{
-			m_AllowedTransitionsTo &= ~BIT(stateIndex);
-		}
-		bool CanTransitTo(uint32_t stateIndex) const
-		{
-			return m_AllowedTransitionsTo & BIT(stateIndex);
-		}
-
-		uint32_t GetID() const { return m_ID; }
-		std::bitset<NumStates> GetAllowedTransitions() const { return m_AllowedTransitionsTo; }
-	private:
-		uint32_t m_ID;
-		std::bitset<NumStates> m_AllowedTransitionsTo;
-	};
-
-	// TODO: use maybe std::bitset instead of uint32_t , it will result in more states
-	template <uint32_t NumStates>
 	class StateMachine
 	{
 	public:
-		State<NumStates>& CreateState()
-		{	
-			bool freeBit = false;
-			uint8_t firstFreeBit = 0;
-			for (uint32_t i = 0; i < NumStates; ++i)
-			{
-				if (!(m_StatesInitialized & BIT(i)))
-				{
-					firstFreeBit = i;
-					freeBit = true;
-					break;
-				}
-			}
-			XYZ_ASSERT(freeBit, "Maximum number of states is ", NumStates);
-
-			State<NumStates> state(firstFreeBit, 0);
-			m_StatesInitialized |= BIT(state.GetID());
-			m_States[state.GetID()] = state;
-			return m_States[state.GetID()];
-		}
-		void DestroyState(uint32_t id)
-		{
-			m_StatesInitialized &= ~BIT(id);
-		}
-
-		bool TransitionTo(uint32_t id)
-		{
-			auto& current = m_States[m_CurrentState];
-			// Check if id of state is in allowed transitions of the current state or check if the current state has allowed any transitions;	
-			XYZ_ASSERT(m_StatesInitialized & BIT(id), "State was not registered in this state machine");
-			if (current.GetAllowedTransitions().test(id))
-			{
-				m_CurrentState = id;
-				return true;
-			}
-			return false;
-		}
-
-		void SetDefaultState(uint32_t id)
-		{
-			XYZ_ASSERT(m_StatesInitialized & BIT(id), "State with id ", id, "is not initialized");
-			m_CurrentState = id;
-		}
-
-		inline const State<NumStates>& GetCurrentState() const 
-		{ 
-			return m_States[m_CurrentState]; 
-		}
-
-		inline State<NumStates>& GetState(uint32_t id) 
-		{ 
-			XYZ_ASSERT(m_StatesInitialized & BIT(id), "State with id ", id, "is not initialized");
-			return m_States[id]; 
-		}
-
-		inline uint32_t GetNumStates() const 
-		{
-			return m_NextFreeBit; 
-		}
-
-		bool IsStateInitialized(uint32_t id) const 
-		{ 
-			return m_StatesInitialized & BIT(id); 
-		}
+		StateMachine(uint32_t defaultId = 0);
+	
+		bool TransitionTo(uint32_t id);
+	
+		void SetState(uint32_t id);
+		void SetAllowTransition(uint32_t fromID, uint32_t toID, bool value);
+	
+		inline bool		CanTransitTo(uint32_t id) const { return m_CurrentState->AllowedTransitions.test(id); }
+		inline uint32_t GetCurrentState()		  const { return m_CurrentState->ID; }
+		
 	private:
-		uint32_t m_CurrentState;
-
-		uint32_t m_StatesInitialized = 0;
-
-		State<NumStates> m_States[NumStates];
+		struct State
+		{
+			std::bitset<NumStates> AllowedTransitions;
+			uint32_t			   ID;
+		};
+	
+		State* m_CurrentState;
+		State  m_States[NumStates];
 	};
+	
+	template<uint32_t NumStates>
+	inline StateMachine<NumStates>::StateMachine(uint32_t defaultId)
+	{
+		m_CurrentState = &m_States[defaultId];
+	}
+	
+	template<uint32_t NumStates>
+	inline bool StateMachine<NumStates>::TransitionTo(uint32_t id)
+	{
+		if (m_CurrentState->AllowedTransitions.test(id))
+		{
+			m_CurrentState = &m_States[id];
+			return true;
+		}
+		return false;
+	}
+	
+	template<uint32_t NumStates>
+	inline void StateMachine<NumStates>::SetState(uint32_t id)
+	{
+		m_CurrentState = &m_States[id];
+	}
+	
+	template<uint32_t NumStates>
+	inline void StateMachine<NumStates>::SetAllowTransition(uint32_t fromID, uint32_t toID, bool value)
+	{
+		m_States[fromID].AllowedTransitions.set(toID, value);
+	}
 
 
 }
