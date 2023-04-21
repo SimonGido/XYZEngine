8ec56f2abf67561f125959bcd1c81239cde5b14c
Šimon Gido <simongido1@gmail.com>
2023-04-21 17:41:32 +0200

    better voxel rendering plus bugfix

--- XYZEditor/Resources/Shaders/RaymarchShader.glsl
+++ XYZEditor/Resources/Shaders/RaymarchShader.glsl
@@ -55,7 +55,7 @@ Ray CreateRay(vec2 coords)
 	Ray ray;
 	ray.Origin = (u_Uniforms.Transform * rayOrigin).xyz;
 
-	ray.Direction = vec3(u_InverseView * u_Uniforms.Transform * vec4(normalize(vec3(target) / target.w), 0)); // World space
+	ray.Direction = vec3(u_Uniforms.Transform * u_InverseView * vec4(normalize(vec3(target) / target.w), 0)); // World space
 	ray.Direction = normalize(ray.Direction);
 
 	return ray;
@@ -73,12 +73,11 @@ uint Index3D(ivec3 index)
 }
 
 
-bool IsInsideChunk(ivec3 voxel)
+bool IsValidVoxel(ivec3 voxel)
 {
-	vec3 current_voxel = vec3(voxel.x, voxel.y, voxel.z);
-	return ((current_voxel.x < float(u_Uniforms.Width ) && current_voxel.x > 0)
-		 && (current_voxel.y < float(u_Uniforms.Height) && current_voxel.y > 0)
-		 && (current_voxel.z < float(u_Uniforms.Depth)  && current_voxel.z > 0));
+	return ((voxel.x < u_Uniforms.Width  && voxel.x > 0)
+		 && (voxel.y < u_Uniforms.Height && voxel.y > 0)
+		 && (voxel.z < u_Uniforms.Depth  && voxel.z > 0));
 }
 
 
@@ -92,36 +91,29 @@ vec4 VoxelToColor(uint voxel)
 
 	return color;
 }
-
-vec4 RayMarch(vec3 rayOrig, vec3 rayDir)
+uint VoxelAlpha(uint voxel)
 {
-	vec4 defaultColor = vec4(0.3, 0.2, 0.7, 1.0);
-	vec3 origin = rayOrig;
-	vec3 direction = rayDir;
+	return bitfieldExtract(voxel, 24, 8) / 255;
+}
 
-	ivec3 current_voxel = ivec3(floor(origin / u_Uniforms.VoxelSize));
-	
-	ivec3 step = ivec3(
-		(direction.x > 0.0) ? 1 : -1,
-		(direction.y > 0.0) ? 1 : -1,
-		(direction.z > 0.0) ? 1 : -1
-	);
-	vec3 next_boundary = vec3(
-		float((step.x > 0) ? current_voxel.x + 1 : current_voxel.x) * u_Uniforms.VoxelSize,
-		float((step.y > 0) ? current_voxel.y + 1 : current_voxel.y) * u_Uniforms.VoxelSize,
-		float((step.z > 0) ? current_voxel.z + 1 : current_voxel.z) * u_Uniforms.VoxelSize
-	);
+struct RaymarchResult
+{
+	vec4 color;
+	uint alpha;
+	vec3 t_max;
+	ivec3 current_voxel;
+	uint traverseCount;
+};
 
-	vec3 t_max = (next_boundary - origin) / direction; // we will move along the axis with the smallest value
-	vec3 t_delta = u_Uniforms.VoxelSize / direction * vec3(step);
-	vec3 normal;
-	uint voxel = 0;
+
+RaymarchResult RayMarch(vec3 t_max, vec3 t_delta, ivec3 current_voxel, ivec3 step, uint maxTraverses)
+{
+	RaymarchResult result;
+	result.color = vec4(0.3, 0.2, 0.7, 1.0);
 	uint i = 0;
-
-	bool first = true;
-	vec4 result = defaultColor;
-	do 
+	for (i = 0; i < maxTraverses; i++)
 	{
+		vec3 normal;
 		if (t_max.x < t_max.y && t_max.x < t_max.z) 
 		{
 			normal = vec3(float(-step.x), 0.0, 0.0);
@@ -140,45 +132,72 @@ vec4 RayMarch(vec3 rayOrig, vec3 rayDir)
 			t_max.z += t_delta.z;
 			current_voxel.z += step.z;		
 		}
-		
-		// Is inside voxel area
-		if (IsInsideChunk(current_voxel))
+		if (IsValidVoxel(current_voxel))
 		{
-			voxel = Voxels[Index3D(current_voxel)];
+			uint voxel = Voxels[Index3D(current_voxel)];
 			if (voxel != 0)
 			{
 				float light = dot(-u_LightDirection.xyz, normal);
 				vec4 voxelColor = VoxelToColor(voxel);
 				vec3 color = voxelColor.rgb * u_LightColor.rgb * light;
-					
-				if (voxelColor.a < 1.0)
-				{
-					voxel = 0;
-				}
-
-				if (first)
-				{
-					first = false;
-					result = vec4(color.rgb, voxelColor.a);
-				}
-				else
-				{
-					result.rgb = mix(result.rgb, color, 1.0 - result.a);
-				}
+				result.alpha = VoxelAlpha(voxel);
+				result.color = vec4(color.rgb, voxelColor.a);
+				break;
 			}
 		}
-		i += 1;
-	} 
-	while (voxel == 0 && i < u_Uniforms.MaxTraverse);
+	}
+	result.traverseCount = i;
+	result.t_max = t_max;
+	result.current_voxel = current_voxel;
+
+	return result;
+}
+
+vec4 RayMarch(vec3 origin, vec3 direction)
+{
+	vec4 defaultColor = vec4(0.3, 0.2, 0.7, 1.0);
+	ivec3 current_voxel = ivec3(floor(origin / u_Uniforms.VoxelSize));
 	
-	if (voxel == 0 && first == false) // We did not hit opaque voxel => blend with background color
+	ivec3 step = ivec3(
+		(direction.x > 0.0) ? 1 : -1,
+		(direction.y > 0.0) ? 1 : -1,
+		(direction.z > 0.0) ? 1 : -1
+	);
+	vec3 next_boundary = vec3(
+		float((step.x > 0) ? current_voxel.x + 1 : current_voxel.x) * u_Uniforms.VoxelSize,
+		float((step.y > 0) ? current_voxel.y + 1 : current_voxel.y) * u_Uniforms.VoxelSize,
+		float((step.z > 0) ? current_voxel.z + 1 : current_voxel.z) * u_Uniforms.VoxelSize
+	);
+
+	vec3 t_max = (next_boundary - origin) / direction; // we will move along the axis with the smallest value
+	vec3 t_delta = u_Uniforms.VoxelSize / direction * vec3(step);	
+
+
+	uint remainingTraverses = u_Uniforms.MaxTraverse;
+	RaymarchResult hit = RayMarch(t_max, t_delta, current_voxel, step, remainingTraverses);
+	vec4 result = hit.color;
+	remainingTraverses -= hit.traverseCount;
+
+	bool opaqueHit = false;
+	while (remainingTraverses != 0)
+	{
+		if (hit.alpha == 1)
+		{
+			opaqueHit = true;
+			break;
+		}
+		hit = RayMarch(hit.t_max, t_delta, hit.current_voxel, step, remainingTraverses);
+		result.rgb = mix(result.rgb, hit.color.rgb, 1.0 - result.a);
+		remainingTraverses -= hit.traverseCount;
+	}
+
+	if (!opaqueHit) // We did not hit opaque voxel => blend with background color
 	{
 		result.rgb = mix(result.rgb, defaultColor.rgb, 1.0 - result.a);
 	}
 	return result;
-
-	return defaultColor;
 }
+
 
 
 layout(local_size_x = 32, local_size_y = 32, local_size_z = 1) in;
